3e1e2875c67046dea09826c081786045c812d7dd
naveedahmed286 <00305cheema@gmail.com>
2025-12-13 08:42:02 +0500

    Basic apify_client.py

--- app/apify_client.py
+++ app/apify_client.py
@@ -1,1 +1,9 @@
-[PASTE THE ENTIRE CODE ABOVE HERE]
+import os
+from app.logger import logger
+
+class ApifyClient:
+    def __init__(self):
+        self.api_token = os.getenv('APIFY_TOKEN')
+        logger.info('Apify client ready')
+
+apify_client = ApifyClient()
